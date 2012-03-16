2274679ae7d753dff77e6c3a1e96b4ba5f37ee60
eugene <eugene@eugene-H61M-USB3-B3.(none)>
2012-03-16 15:23:34 +0600

    impr salepointid

--- xml_generator/api.py
+++ xml_generator/api.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 class SalepointResource(ModelResource):
     #related_name  : helps to populate reverse relations; must be a field on the other Resource
     #full=True чтобы видеть начинку offers, а не просто их resource_uri
-    offers = fields.ToManyField('xml_generator.api.OfferResource','offer_set', related_name='salepoint', full=True)
+    #offers = fields.ToManyField('xml_generator.api.OfferResource','offer_set', related_name='salepoint', full=True)
     class Meta:
         queryset = Salepoint.objects.all()
         resource_name = 'salepoint'
@@ -17,8 +17,12 @@ class SalepointResource(ModelResource):
 
 
 class OfferResource(ModelResource):
-    salepoint = fields.ToOneField(SalepointResource, 'salepoint')
+    #salepoint = fields.ToOneField(SalepointResource, 'salepoint')
     class Meta:
         queryset = Offer.objects.all()
         resource_name = 'offer'
+
+    def dehydrate(self, bundle):
+        bundle.data['salepoint_id'] = bundle.obj.salepoint.id
+        return bundle
 
