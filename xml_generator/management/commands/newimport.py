b6f42fdc7ea30b5fed51ff39870ee0ce8ff8654c
eugene <eugene@eugene-H61M-USB3-B3.(none)>
2012-05-12 19:40:31 +0700

    impr timezone

--- xml_generator/management/commands/newimport.py
+++ xml_generator/management/commands/newimport.py
@@ -16,32 +16,32 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            req = urllib2.Request("http://v-zabote.ru/api/v1/organization/?format=json&limit=0", None, {'user-agent':'syncstream/vimeo'})
-
-            opener = urllib2.build_opener()
-            f = opener.open(req)
-
-            s = simplejson.load(f)
-
-            for obj in s['objects']:
-                sp = Organization(name=  obj['name'], pk = obj['id'])
-                sp.save()
-
-
-            req = urllib2.Request("http://v-zabote.ru/api/v1/station/?format=json&limit=0", None, {'user-agent':'syncstream/vimeo'})
-
-            opener = urllib2.build_opener()
-            f = opener.open(req)
-
-            s = simplejson.load(f)
-
-            for obj in s['objects']:
-                org = Organization.objects.get(pk=obj['organization_id'])
-                sp = Salepoint(pk=int(obj['id']), name=obj['name'], address=obj['address'], city=obj['city'],
-                    latitude=obj['lat'], longitude=obj['lon'], status = obj['status'], is_new = False,  is_redundant=False,
-                    organ=org,  point_type=obj['point_type'])
-                #pricelist_name=obj['pricelist_name'], pricelist_url=obj['pricelist_url'],
-                sp.save()
+#            req = urllib2.Request("http://v-zabote.ru/api/v1/organization/?format=json&limit=0", None, {'user-agent':'syncstream/vimeo'})
+#
+#            opener = urllib2.build_opener()
+#            f = opener.open(req)
+#
+#            s = simplejson.load(f)
+#
+#            for obj in s['objects']:
+#                sp = Organization(name=  obj['name'], pk = obj['id'])
+#                sp.save()
+#
+#
+#            req = urllib2.Request("http://v-zabote.ru/api/v1/station/?format=json&limit=0", None, {'user-agent':'syncstream/vimeo'})
+#
+#            opener = urllib2.build_opener()
+#            f = opener.open(req)
+#
+#            s = simplejson.load(f)
+#
+#            for obj in s['objects']:
+#                org = Organization.objects.get(pk=obj['organization_id'])
+#                sp = Salepoint(pk=int(obj['id']), name=obj['name'], address=obj['address'], city=obj['city'],
+#                    latitude=obj['lat'], longitude=obj['lon'], status = obj['status'], is_new = False,  is_redundant=False,
+#                    organ=org,  point_type=obj['point_type'])
+#                #pricelist_name=obj['pricelist_name'], pricelist_url=obj['pricelist_url'],
+#                sp.save()
 
 
 
@@ -60,7 +60,7 @@ class Command(BaseCommand):
                         for _sp in obj['salepoints']:
                             sp = Salepoint.objects.get(pk=_sp)
                             pr = Product.objects.get(source_code=int(obj['source_code']), source_type=obj['source_type'])
-                            off = Offer(product=pr, salepoint=sp, price=obj['price'], created=datetime.fromtimestamp(obj['timestamp']))
+                            off = Offer(product=pr, salepoint=sp, price=obj['price'], created=datetime.fromtimestamp(obj['timestamp']).replace(tzinfo=utc))
                             off.save()
                     except Exception as e:
                         print str(e)
