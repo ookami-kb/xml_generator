73774e01dfe4cd2feea447d0f9142ed14226f23a
eugene <eugene@eugene-H61M-USB3-B3.(none)>
2012-05-12 17:58:28 +0700

    impr statistic

--- xml_generator/analytics/admin_views.py
+++ xml_generator/analytics/admin_views.py
@@ -2,12 +2,15 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from xml_generator.models import Salepoint, Offer
+from xml_generator.models import *
 import datetime
 from django.db.models import Count
 from django.contrib.auth.models import User
-
+from django.db.models import Q
 from .forms import DateRange, UserSelect
+
+def ValuesQuerySetToDict(vqs):
+    return [item for item in vqs]
 
 @staff_member_required
 def salepoints_stat(request):
@@ -24,7 +27,55 @@ def salepoints_stat(request):
         uid = users.cleaned_data['user']
     else:
         uid = None
-        
+    _off = []
+    if not uid:
+        for uid in User.objects.exclude(username__in=['admin', 'fuel', 'test_fuel', 'test']):
+            for t in Task.objects.filter(date_to_execute__gte=start, date_to_execute__lte=stop, is_pattern=False, user=uid):
+                for sp in t.salepoint.all():
+                    _o = Offer.objects.filter(created__gte=start, created__lte=stop, created__day=t.date_to_execute.day + 1,\
+                        created__month=t.date_to_execute.month, created__year=t.date_to_execute.year,\
+                        salepoint=sp).select_related()
+                    #for o in _o:
+                    #    print o.created
+                    # print  '\n !  '
+                    _count = _o.count()
+                    #print _count
+                    # print _o
+                    #_a = ValuesQuerySetToDict(_o)
+                    #_o['offers_count'] = _count
+                    _off.append({
+                        'salepoint__user' : uid,
+                        'salepoint__id' : sp.pk,
+                        'salepoint__name' : sp.name,
+                        'salepoint__address' : sp.address,
+                        'created' : str(t.date_to_execute.day) + '.' + str(t.date_to_execute.month) + '.' + str(t.date_to_execute.year),
+                        'offers_count' : _count,
+                        })
+    else:
+        for t in Task.objects.filter(date_to_execute__gte=start, date_to_execute__lte=stop, is_pattern=False, user=uid):
+            for sp in t.salepoint.all():
+                _o = Offer.objects.filter(created__gte=start, created__lte=stop, created__day=t.date_to_execute.day + 1,\
+                    created__month=t.date_to_execute.month, created__year=t.date_to_execute.year,\
+                    salepoint=sp).select_related()
+                #for o in _o:
+                #    print o.created
+               # print  '\n !  '
+                _count = _o.count()
+                #print _count
+               # print _o
+                #_a = ValuesQuerySetToDict(_o)
+            #_o['offers_count'] = _count
+                _off.append({
+                    'salepoint__user' : uid,
+                    'salepoint__id' : sp.pk,
+                    'salepoint__name' : sp.name,
+                    'salepoint__address' : sp.address,
+                    'created' : str(t.date_to_execute.day) + '.' + str(t.date_to_execute.month) + '.' + str(t.date_to_execute.year),
+                    'offers_count' : _count,
+                })
+            #print _off
+
+    '''
     filters = {}
     offers = Offer.objects.all()
     if start:
@@ -36,14 +87,16 @@ def salepoints_stat(request):
     
     if filters:
         offers = offers.filter(**filters)
-        
+
+
+
     offers = offers.values('salepoint__user', 'salepoint__id', 'salepoint__name', 'created').annotate(offers_count=Count('id'))
     
     print offers
-        
+    '''
     return render_to_response(
         "admin/salepoints_stat.djhtml",
-        {'offers' : offers,
+        {'offers' : _off,
          'date_range': date_range,
          'users_form': users},
         RequestContext(request, {}),
@@ -59,17 +112,47 @@ def users_stat(request):
         start = datetime.date.fromtimestamp(0)
         stop = datetime.date.today() + datetime.timedelta(days=1)
         
-    users = User.objects.all()
-    
-    # кол-во загруженных предложений в заданном промежутке
-    users = users.extra(select={'offers_count': 'SELECT COUNT(*) FROM xml_generator_offer WHERE xml_generator_offer.salepoint_id IN (SELECT DISTINCT id from xml_generator_salepoint WHERE xml_generator_salepoint.user_id = auth_user.id) AND xml_generator_offer.created >= %s and xml_generator_offer.created <= %s'},
-                        select_params=(start, stop))
-    
-    # кол-во мониторенных магазинов в заданном промежутке
-    users = users.extra(select={'salepoints_count': 'SELECT COUNT(*) FROM (SELECT * FROM xml_generator_offer WHERE xml_generator_offer.salepoint_id IN (SELECT id from xml_generator_salepoint WHERE xml_generator_salepoint.user_id = auth_user.id) AND xml_generator_offer.created >= %s and xml_generator_offer.created <= %s GROUP BY xml_generator_offer.salepoint_id, xml_generator_offer.created)'},
-                        select_params=(start, stop))
-    
-    print users.query
+    users = User.objects.exclude(username__in=['admin', 'fuel', 'test_fuel', 'test'])
+
+
+    _users = []
+    for u in users:
+        # кол-во загруженных предложений в заданном промежутке
+        _offer_count = 0
+        # кол-во мониторенных магазинов в заданном промежутке
+        _sp_count = 0
+        '''
+        for t in Task.objects.filter(Q(date_to_execute__gte=start) & Q(date_to_execute__lte=stop) & Q(is_pattern=False) & Q(user=u)):
+
+            _offer_count +=  Offer.objects.filter(Q(created__gte=start) & Q(created__lte=stop) & Q(created__day=t.date_to_execute.day) &\
+                                           Q(created__month=t.date_to_execute.month) & Q(created__year=t.date_to_execute.year) &\
+                                            Q(salepoint__in=t.salepoint.all())).count()
+
+            #if t.salepoint in
+            _sp_count += t.salepoint.count()
+
+        '''
+
+
+        for t in Task.objects.filter(date_to_execute__gte=start, date_to_execute__lte=stop, is_pattern=False, user=u):
+
+
+            _off = Offer.objects.filter(created__gte=start, created__lte=stop, created__day=t.date_to_execute.day + 1, \
+                                                  created__month=t.date_to_execute.month, created__year=t.date_to_execute.year,\
+                                                    salepoint__in=t.salepoint.all())
+            print _off
+            _offer_count += _off.count()
+            #if t.salepoint in
+            _sp_count += t.salepoint.count()
+            print t.user.username
+        print u.username + ' ' + str(u.pk) + ' ' + str(_offer_count) + ' ' + str(_sp_count) + '\n'
+
+        _users.append({
+            'user': u,
+            'offers_count': _offer_count,
+            'sps_count' : _sp_count,
+        })
+    #print users.query
     
     # выбираем кол-во предложений, которые пользователи загрузили
     # в заданном промежутке
@@ -77,7 +160,7 @@ def users_stat(request):
 
     return render_to_response(
         "admin/users_stat.djhtml",
-        {'users' : users,
+        {'users' : _users,
          'date_range': date_range},
         RequestContext(request, {}),
     )
